973b3935209cb9eb8c9a50a67f7522a71aff180d
sakib12345 <sakib35-1680@diu.edu.bd>
2021-10-13 15:49:52 +0600

    first api made

---
+++ index.js
@@ -0,0 +1,13 @@
+//dependencies
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res => {
+    res.send('Hello sakib')
+}))
+
+app.listen(port, ()=>{
+    console.log(`Listening port on ${port}`)
+})
